2e4ab1f1f95e50c908e63ca2ca66c562455a0579
unknown <lorenzopenalver3@gmail.com>
2023-09-11 11:18:34 -0300

    Primer commit

---
+++ Code/index.js
@@ -0,0 +1,86 @@
+const featureList = document.querySelector(".features-list")
+const featureItems = document.querySelector(".feature-items")
+const menuOptions = document.querySelector(".menu-container")
+const companyOptions = document.querySelector(".company-options")
+const companyItems = document.querySelector(".company-sub-items")
+const arrows = document.querySelector(".arrows")
+const arrows2 = document.querySelector(".arrows2")
+const menuIcon = document.querySelector(".menu-icon")
+const menuCloseIcon = document.querySelector(".menu-close")
+
+
+menuIcon.addEventListener("click", openMenu)
+menuCloseIcon.addEventListener("click", openMenu)
+featureList.addEventListener("click", featrueConfig)
+companyOptions.addEventListener("click", companyConfig)
+arrows.addEventListener("click", featrueConfig)
+arrows2.addEventListener("click", companyConfig)
+
+function openMenu() {
+    if(menuOptions.classList.contains("inactive")){
+        menuOptions.classList.remove("inactive")
+        arrowImg.src = "./images/icon-arrow-down.svg"
+        arrowImg2.src = "./images/icon-arrow-down.svg"
+    }else if (!menuOptions.classList.contains("inactive")){
+        menuOptions.classList.add("inactive")
+        featureItems.classList.add("inactive")
+        companyItems.classList.add("inactive")
+        arrowImg.src = "./images/icon-arrow-down.svg"
+        arrowImg2.src = "./images/icon-arrow-down.svg"
+    }
+ 
+}
+function featrueConfig() {
+    if(featureItems.classList.contains("inactive")){
+        featureItems.classList.remove("inactive")
+        arrowImg.src = "./images/icon-arrow-up.svg"
+    }else if (!featureItems.classList.contains("inactive")){
+        featureItems.classList.add("inactive")
+        arrowImg.src = "./images/icon-arrow-down.svg"
+    }
+}
+function companyConfig() {
+    if(companyItems.classList.contains("inactive")){
+        companyItems.classList.remove("inactive")
+        arrowImg2.src = "./images/icon-arrow-up.svg"
+    }else if (!companyItems.classList.contains("inactive")){
+        companyItems.classList.add("inactive")
+        arrowImg2.src = "./images/icon-arrow-down.svg"
+    }
+}
+companyOptions.addEventListener( 'ondblclick', function(event) {  
+    event.preventDefault();  
+    event.stopPropagation(); 
+  },  true 
+);
+window.addEventListener("resize", closeGeneral)
+function closeGeneral() {
+    if(window.innerWidth > 740){
+        featureItems.classList.add("inactive")
+        menuOptions.classList.add("inactive")
+        companyItems.classList.add("inactive")
+        arrowImg.src = "./images/icon-arrow-down.svg"
+        arrowImg2.src = "./images/icon-arrow-down.svg"
+    }
+}
+
+const arrowImg = document.createElement("img")
+arrowImg.src = "./images/icon-arrow-down.svg"
+arrowImg.style = ` 
+width: 15px;
+height: 10px;
+cursor: pointer;
+`
+
+
+const arrowImg2 = document.createElement("img")
+arrowImg2.src = "./images/icon-arrow-down.svg"
+arrowImg2.style = ` 
+width: 15px;
+height: 10px;
+cursor: pointer;
+`
+
+
+arrows.append(arrowImg)
+arrows2.append(arrowImg2)
---
+++ README.md
@@ -0,0 +1,57 @@
+![](https://i.ibb.co/j8rds7B/Logo-5.png)
+
+# Dropdown Menu
+
+![Vista previa](./design/desktop-preview.jpg)
+
+## Desafio 💪
+
+Su desafío es construir esta sección de introducción con navegación desplegable y lograr que se parezca lo más posible al diseño.
+
+Los estudiantes deben ser capaces de:
+
+- Ver los menús desplegables en el escritorio y el móvil al interactuar con los enlaces de navegación.
+- Ver el diseño óptimo para el contenido dependiendo del tamaño de pantallas sugeridos.
+- En vista mobile lograr adaptar el menu de navegacion usando Javascript.
+- Inicializar tu proyecto como repositorio público en GitHub. La creación de un repositorio hará que sea más fácil compartir su código con la comunidad si necesita ayuda.
+
+```
+En la carpeta "design" tendras las vistas que debes replicar y en la carpeta "images" los recursos.
+```
+
+## Proceso
+
+### Construir con:
+
+- Semantica HTML
+- CSS
+- Flexbox
+- CSS Grid
+- Mobile-first
+- Javascript
+
+# Guía de estilo de interfaz
+
+## Disposicion aproximada
+
+- Mobile: 480px
+- Desktop: 1440px
+
+## Colores
+
+### Neutral
+
+- Casi blanco: #F9F9F9
+- Gris medio: #686868
+- Casi negro: #141414
+
+## Tipografia
+
+### Body
+
+- Font size (parrafos): 18px
+
+### Font
+
+- Family: [Epilogue](https://fonts.google.com/specimen/Epilogue)
+- Weights: 500, 700
